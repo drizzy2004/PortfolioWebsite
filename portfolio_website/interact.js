8c9848327fe6058cd59fa643e915188fd8a5f2ef
drizzy2004 <157053650+drizzy2004@users.noreply.github.com>
2026-02-11 16:41:15 -0700

    Updated website's UI for a more modern look and feel

--- portfolio_website/interact.js
+++ portfolio_website/interact.js
@@ -1,18 +1,34 @@
-var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+const sidemenu = document.getElementById("sidemenu");
 
-    function opentab(tabname) {
-
-        for(tablink of tablinks){
-            tablink.classList.remove("active-link");
-        }
-        for(tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
-        }
-
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname, trigger) {
+    for (const tablink of tablinks) {
+        tablink.classList.remove("active-link");
     }
 
-    
-    
+    for (const tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    if (trigger) {
+        trigger.classList.add("active-link");
+    }
+
+    const selectedTab = document.getElementById(tabname);
+    if (selectedTab) {
+        selectedTab.classList.add("active-tab");
+    }
+}
+
+function openmenu() {
+    if (sidemenu) {
+        sidemenu.style.right = "0";
+    }
+}
+
+function closemenu() {
+    if (sidemenu) {
+        sidemenu.style.right = "-260px";
+    }
+}
